7c05353ecbb9146917547b1f44ebbb7b882a5b0d
Reid Tull <reid.a.tull@gmail.com>
2022-11-21 17:12:21 -0500

    google maps, and profile stuff

--- app/src/main/java/com/example/lab_5/MainActivity.java
+++ app/src/main/java/com/example/lab_5/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.lab_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // linked to the button in the XML
+    public void GoToProfileSelect(View view){
+        Intent intent = new Intent(MainActivity.this, ProfileActivty.class);
+        startActivity(intent);
+    }
+
+    // linked to the button in the XML
+    public void OpenGoogleMaps(View view){
+        EditText address = (EditText) findViewById(R.id.AddressEdit);
+
+        Uri gmUrl = Uri.parse("https://www.google.com/maps?q=" + address.getText());
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmUrl);
+
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
     }
 }
--- app/src/main/java/com/example/lab_5/ProfileActivty.java
+++ app/src/main/java/com/example/lab_5/ProfileActivty.java
@@ -2,7 +2,11 @@ package com.example.lab_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivty extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class ProfileActivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    // I link this to the images in the XML
+    public void SelectTeamIcon (View view){
+        Intent intent = new Intent();
+        ImageView selectImage = (ImageView) view;
+
+        intent.putExtra("ImageID", selectImage.getId());
+        setResult(RESULT_OK, intent);
+
+        finish();
     }
 }
