567cdb190c5f33af29659210015c4b8a256f4f7d
Zaid <113944271+Daizon99@users.noreply.github.com>
2022-11-21 17:46:48 -0500

    Update ProfileActivty.java

--- app/src/main/java/com/example/lab_5/ProfileActivty.java
+++ app/src/main/java/com/example/lab_5/ProfileActivty.java
@@ -2,13 +2,44 @@ package com.example.lab_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ProfileActivty extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
+    EditText inputTxt;
+
+    Button submitB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.ProfileActivity);
+
+        inputTxt = findViewById(R.id.inputTxt);
+        submitB = findViewById(R.id.submitB);
+
+        submitB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+
+            public void onClick(view v){
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("txtData", inputTxt.getText().toString);
+                setResult(RESULT_OK,returnIntent);
+                finish();
+            }
+
+        });
+
     }
+
+
 }
