c360e01c69ea4fdb53ad5c2cd4cc29da9ee40c4d
Zaid <113944271+Daizon99@users.noreply.github.com>
2022-11-21 18:02:03 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/lab_5/MainActivity.java
+++ app/src/main/java/com/example/lab_5/MainActivity.java
@@ -11,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+      public void OnOpenInGoogleMaps(View view) {
+        EditText teamAddress = (EditText) findViewById(R.id.teamAddressField);
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddress.getText());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
 }
